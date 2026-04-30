7296bda51ca2e1628b283d90c984976f0ee3b4ca
acongm <acongm@126.com>
2026-04-30 22:08:23 +0800

    feat(ui-bridge): add planned adapter components

---
+++ packages/ui-bridge/src/adapter-components.tsx
@@ -0,0 +1,25 @@
+import { PlannedAntdAdapter } from './adapters/antd';
+
+export function Form() {
+  return <PlannedAntdAdapter componentName="Form" />;
+}
+
+export function Modal() {
+  return <PlannedAntdAdapter componentName="Modal" />;
+}
+
+export function Drawer() {
+  return <PlannedAntdAdapter componentName="Drawer" />;
+}
+
+export function DatePicker() {
+  return <PlannedAntdAdapter componentName="DatePicker" />;
+}
+
+export function Select() {
+  return <PlannedAntdAdapter componentName="Select" />;
+}
+
+export function Upload() {
+  return <PlannedAntdAdapter componentName="Upload" />;
+}
